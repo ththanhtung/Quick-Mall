9fcfc1dc83b06e438ed8eed4d0670c1a45fec377
Thanh Tung <nguyenththanhtung317@gmail.com>
2022-02-27 21:18:46 +0700

    create route for cart page

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export {default as About} from './About'
 export {default as ProductDetail} from './ProductDetail'
 export {default as Home} from './Home'
+export {default as Cart} from './Cart'
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {Home} from './Pages'
+import {Home, Cart} from './Pages'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +10,7 @@ ReactDOM.render(
       <Routes>
         <Route path='/' element={<App/>}>
           <Route index element={<Home/>}/>
+          <Route path='cart' element={<Cart/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
