5e68554a2a4767656a24b53334db21e89b88cd08
Thanh Tung <nguyenththanhtung317@gmail.com>
2022-03-01 00:25:39 +0700

    create cart total price component

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {CartItem} from '../Components';
+import {CartItem, CartTotalPrice} from '../Components';
 import styles from '../styles/PageLayout/CartPage.module.scss';
 import clsx from 'clsx';
 
@@ -9,6 +9,15 @@ const Cart = () => {
         <div className={clsx(styles.containerCartItems)}>
           <CartItem/>
           <CartItem/>
+          <CartItem/>
+          <CartItem/>
+          <CartItem/>
+          <CartItem/>
+          <CartItem/>
+          <CartItem/>
+          <CartItem/>
+          <CartItem/>
+          <CartTotalPrice/>
         </div>
     </main>
   )
---
+++ src/components/CartTotalPrice.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const cartTotalPrice = () => {
+  return (
+    <div className="container-cart-total-price">
+      <div className="container-center-cart-total-price">
+        <div className="total-price-amount-product">
+          <h4 className="title-total-price">total price:</h4>
+          <h4 className="total-price">$100.000</h4>
+          <h4 className="total-amount-product">3 selected product(s)</h4>
+        </div>
+        <div className="container-checkout">
+          <button className="btn-checkout">
+            checkout
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default cartTotalPrice
