5b096be99c781843a53c8c4178cdc67aa68a5f05
Thanh Tung <nguyenththanhtung317@gmail.com>
2022-03-08 19:49:09 +0700

    update erro page style

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import clsx from 'clsx'
+import styles from '../styles/PageLayout/Error.module.scss'
+import {BiSad} from 'react-icons/bi'
 
 const Error = () => {
   return (
-    <main>
-        error
+    <main className={clsx(styles.mainContainer)}>
+        <div className={clsx(styles.container)}>
+            <h1 className={clsx(styles.h1Error)}>page not found</h1>
+            <p className={clsx(styles.pError)}>error code: 404</p>
+            <BiSad className={clsx(styles.iconSad)}/>
+        </div>
     </main>
   )
 }
 
-export default Error
+export default React.memo(Error)
