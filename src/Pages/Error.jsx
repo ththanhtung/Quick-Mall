02fc958a426c452821186cd0f6fae28a9df304a2
Thanh Tung <nguyenththanhtung317@gmail.com>
2022-03-08 20:01:08 +0700

    add route to main page and update style to error page

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import clsx from 'clsx'
 import styles from '../styles/PageLayout/Error.module.scss'
 import {BiSad} from 'react-icons/bi'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
     <main className={clsx(styles.mainContainer)}>
         <div className={clsx(styles.container)}>
             <h1 className={clsx(styles.h1Error)}>page not found</h1>
-            <p className={clsx(styles.pError)}>error code: 404</p>
+            <Link className={clsx(styles.aError)} to='/'>back to main page</Link>
             <BiSad className={clsx(styles.iconSad)}/>
         </div>
     </main>
