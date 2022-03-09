35794bb71d7cde180ebaab0f23eceaddf478a80c
Thanh Tung <nguyenththanhtung317@gmail.com>
2022-03-09 16:38:54 +0700

    update header style to be able to display number of products in cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,17 +12,28 @@ const Header = () => {
       <div className="header-container flex">
         <div className="branding flex">
           <h1 className="brand-name">commercial web app</h1>
-          <button className='btn-brand-name' onClick={()=>{navigate('/products')}}>
+          <button 
+            className='btn-brand-name' 
+            onClick={()=>{navigate('/products')}}
+            title='back to products page'
+          >
             <img className='brand-logo' src={brandLogo} alt="brand logo" />
           </button>
         </div>
         <div className="container-btn">
-          <button className='logout-btn' onClick={()=>{navigate('/')}}>
-            <BiLogOutCircle className='icon logout-icon'title='logout'/>
-          </button>
-          <button className='cart-btn' onClick={()=>{navigate('cart')}}>
-            <FiShoppingCart className='icon cart-icon'title='go to cart'/>
-          </button>
+          <div className="container-btn1">
+            <button className='logout-btn' onClick={()=>{navigate('/')}}>
+              <BiLogOutCircle className='icon logout-icon'title='log out'/>
+            </button>
+          </div>
+          <div className="container-btn2">
+            <button className='cart-btn' onClick={()=>{navigate('cart')}}>
+              <FiShoppingCart className='icon cart-icon'title='go to cart'/>
+            </button>
+            <div className="amount-container" title='product(s) in cart'>
+              <h5 className='products-amount'>0</h5>
+            </div>
+          </div>
         </div>
       </div>
     </header>
