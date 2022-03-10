3824d655021572744796ecf22802258e70f0498d
Thanh Tung <nguyenththanhtung317@gmail.com>
2022-03-10 16:56:25 +0700

    update product detail page styles

--- src/components/ProductDetailComponent.jsx
+++ src/components/ProductDetailComponent.jsx
@@ -1,37 +1,43 @@
 import React from 'react'
 import CustomSelect from './CustomSelect'
 
-const ProductDetailComponent = ({image, title, price, category, id}) => {
+const ProductDetailComponent = ({image, title, price, category, id, description}) => {
   return (
-    <article className='container-detail-product'>
-        <div className="container-img">
-            <img src={image} alt={title} />
-        </div>
-        <div className="container-center-product">
-            <div className="info-product">
-                <h1 className='name-product'>{title}</h1>
-                <h2 className='price-product'>${price}</h2>
-                <div className="container-minor-info">
-                    <h3 className="availability">availability: in stock</h3>
-                    <h3 className="id-product">product id: {id}</h3>
-                    <h3 className="categories">categories: {category}</h3>
-                </div>
-            </div>-
-            <div className="container-size-color-quantity">
-                <CustomSelect className='select-color' label='color'/>
-                <CustomSelect className='select-size' label={'size'}/>
-                <div className="field-quantity">
-                    <label htmlFor="quantity-id">quantity</label>
-                    <input type="number" />
-                </div>
+    <article className='article-product-detail'>
+        <div className='container-detail-product'>
+            <div className="container-img">
+                <img src={image} alt={title} />
             </div>
-            <div className="container-button">
-                <div className="btn-add-to-cart">
-                    <button>
+            <div className="container-center-product">
+                <div className="info-product">
+                    <h1 className='name-product'>{title}</h1>
+                    <h2 className='price-product'>${price}</h2>
+                    <div className="container-minor-info">
+                        <h3 className="availability">availability: <span className="info">in stock</span></h3>
+                        <h3 className="id-product">product id: <span className="info">{id}</span></h3>
+                        <h3 className="categories">categories: <span className="info">{category}</span></h3>
+                    </div>
+                </div>
+                <div className="container-size-color-quantity">
+                    <CustomSelect className='select-color' label='color'/>
+                    <CustomSelect className='select-size' label={'size'}/>
+                    <div className="field-quantity">
+                        <label htmlFor="quantity-id">quantity</label>
+                        <input id='quantity-id' type="number" />
+                    </div>
+                </div>
+                <div className="container-button">
+                    <button className="btn-add-to-cart">
                         add to cart
                     </button>
                 </div>
             </div>
+        </div>
+        <div className="container-desc">
+            <h2>description:</h2>
+            <p className="desc">
+                {description}
+            </p>
         </div>
     </article>
   )
